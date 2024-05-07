3800c94084541d31b50303457d0b0f379ae4cbae
GhostAlpha24 <163915613+GhostAlpha24@users.noreply.github.com>
2024-05-06 19:04:06 -0600

    Add files via upload

---
+++ lazowhile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1; // Inicializamos la variable i con 1
+
+    // Mientras i sea menor o igual a 9, se ejecutará el bucle
+    while (i <= 9) {
+        cout << i << " "; // Imprimimos el valor de i seguido de un espacio en blanco
+        i++; // Incrementamos i en 1 en cada iteración
+    }
+
+    cout << endl; // Imprimimos un salto de línea al final
+    return 0; // Indicamos que el programa finalizó correctamente
+}
---
+++ tablas.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i =1, num = 12;
+    while (i<13)
+    {
+        cout << num << " x " << i << " = " << 12*i << "\n";
+        i++;
+    }
+    return 0;
+
+}   
